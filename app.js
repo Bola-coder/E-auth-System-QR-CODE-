4bffb0251c8d5108a0e176d403cd104c563fc4cb
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2023-03-01 17:38:14 +0100

    Added cors for access from everywhere (for now)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/product");
 const errorController = require("./controllers/errorControlelr");
 
 const app = express();
+app.use(cors());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
